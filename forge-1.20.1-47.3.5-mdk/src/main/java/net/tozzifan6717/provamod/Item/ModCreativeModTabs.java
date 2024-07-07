e187bb3d12108cffac6ce02eac5d87b629daf28d
TheKami666 <notmilos666@gmail.com>
2024-07-08 01:53:42 +0200

    .

--- forge-1.20.1-47.3.5-mdk/src/main/java/net/tozzifan6717/provamod/Item/ModCreativeModTabs.java
+++ forge-1.20.1-47.3.5-mdk/src/main/java/net/tozzifan6717/provamod/Item/ModCreativeModTabs.java
@@ -22,10 +22,18 @@ public class ModCreativeModTabs {
             () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.ZAFFIRO.get()))
                     .title(Component.translatable("creativetab.prova_tab"))
                     .displayItems((pParameters, pOutput) -> {
+                        //formula: pOutput.accept(ModTipo.OGGETTO.get());
                         pOutput.accept(ModItems.ZAFFIRO.get());
                         pOutput.accept(ModItems.ZAFFIRO_PURO.get());
+
                         pOutput.accept(ModBlocks.ZAFFIRO_BLOCCO.get());
                         pOutput.accept(ModBlocks.ZAFFIRO_GREZZO_BLOCCO.get());
+
+                        pOutput.accept(ModBlocks.ZAFFIRO_MINERALE.get());
+                        pOutput.accept(ModBlocks.ZAFFIRO_MINERALE_DEEPSLATE.get());
+                        pOutput.accept(ModBlocks.ZAFFIRO_MINERALE_NETHER.get());
+                        pOutput.accept(ModBlocks.ZAFFIRO_MINERALE_END.get());
+
                     })
                     .build());
 
--- forge-1.20.1-47.3.5-mdk/src/main/java/net/tozzifan6717/provamod/block/ModBlocks.java
+++ forge-1.20.1-47.3.5-mdk/src/main/java/net/tozzifan6717/provamod/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.tozzifan6717.provamod.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,10 +23,21 @@ public class ModBlocks {
     //crea il Blocco:
     //[...].copy(Blocks.BloccoDaCopiare.[altri parametri, esempio: .sound(IRON_BLOCK)]...)));
     public static final RegistryObject<Block> ZAFFIRO_BLOCCO = registerBlock("zaffiro_blocco",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
     public static final RegistryObject<Block> ZAFFIRO_GREZZO_BLOCCO = registerBlock("zaffiro_grezzo_blocco",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+    //crea un blocco ORE (minerale).
+    public static final RegistryObject<Block> ZAFFIRO_MINERALE = registerBlock("zaffiro_minerale",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE).strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    public static final RegistryObject<Block> ZAFFIRO_MINERALE_DEEPSLATE = registerBlock("zaffiro_minerale_deepslate",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    public static final RegistryObject<Block> ZAFFIRO_MINERALE_NETHER = registerBlock("zaffiro_minerale_nether",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK).strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    public static final RegistryObject<Block> ZAFFIRO_MINERALE_END = registerBlock("zaffiro_minerale_end",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+
 
     //questi indicano che parliamo di un blocco, NON TOCCARE.
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
